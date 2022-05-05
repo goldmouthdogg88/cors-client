000d667579866fe203bf355eb99b0848fd212ccf
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-05-05 00:36:23 -0400

    A client to complement the server and browser extension

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+import Card from "./components/card/Card.component";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  callAPI() {
+    fetch("https://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }))
+      .catch((err) => err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h1>Welcome to React</h1>
+        <h2 style={{ "margin-bottom": 130 }}>
+          apiResponse: {this.state.apiResponse}
+        </h2>
+        <Card />
+      </div>
+    );
+  }
 }
 
 export default App;
